ab831f8efd7b578defee1d378a5d6c05a9ca96fd
Ben Prejean <benprejean@live.com>
2021-01-30 07:08:15 -0500

    Completed module

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -34,7 +34,18 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
-
+	accessToken := req.Header.Get("X-ACCESS-TOKEN")
+	isValidToken := data.IsAccessTokenValid(accessToken)
+	if !isValidToken {
+		res.WriteHeader(http.StatusUnauthorized)
+	} else {
+		err := data.AddNewWinner(req.Body)
+		if err != nil {
+			res.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
+		res.WriteHeader(http.StatusCreated)
+	}
 }
 
 // WinnersHandler is the dispatcher for all /winners URL
